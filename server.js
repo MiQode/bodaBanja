a4abea7c6c611c56ec31e4b6e6b55b3495e5850a
Mike Rembo <rembomike@gmail.com>
2020-03-06 01:51:58 -0800

    Added the basic form inputs for testing request

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('Hello')
+const express = require('express');
+const server = express();
+
+server.listen(3000, function(){
+    console.log('listening on 3000')
+})
+
+server.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html')
+})
